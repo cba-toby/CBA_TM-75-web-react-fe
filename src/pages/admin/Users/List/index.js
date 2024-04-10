567ffe4c05a910b89a534fbab77351c40520d2ef
Phan Thế Trung (Toby) <toby@cybridge.jp>
2024-04-10 13:59:54 +0700

    Feature/Pagination-and-search

--- src/pages/admin/Users/List/index.js
+++ src/pages/admin/Users/List/index.js
@@ -2,28 +2,52 @@ import { useEffect, useState } from "react";
 import axiosClient from "../../../../axios-client";
 import { Link } from "react-router-dom";
 import { useStateContext } from "../../../../context/ContextProvider";
+import ReactPaginaet from "react-paginate";
+import PaginationComponent from "../../../../components/Pagination";
 
 function Users() {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
   const { setNotification } = useStateContext();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pageCount, setPageCount] = useState(10);
+  const [search, setSearch] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
 
   useEffect(() => {
-    getUsers();
+    getUsers(currentPage);
   }, []);
 
-  const getUsers = () => {
-    setLoading(true);
+  const handlePageChange = (selectedItem) => {
+    const newPage = selectedItem.selected + 1;
+    setCurrentPage(newPage);
+    getUsers(newPage);
+  };
+
+  const getUsers = (page = currentPage, searchValue = null) => {
+    let url = `/admin/users?page=${page}`;
+    if (searchValue) {
+      url += `&search=${searchValue}`;
+    }
     axiosClient
-      .get("/admin/users")
+      .get(url)
       .then(({ data }) => {
-        console.log(data);
         setLoading(false);
         setUsers(data.data);
+        setPageCount(data.meta.last_page);
+        setCurrentPage(data.meta.current_page);
       })
       .catch((error) => {
         setLoading(false);
       });
+  };
+
+  const handleSearch = (value) => {
+    setSearch(value);
+    setLoading(true);
+    clearTimeout(searchTimeout);
+    const timeout = setTimeout(() => getUsers(null, value), 1000);
+    setSearchTimeout(timeout);
   };
 
   const onDeleteUser = (id) => {
@@ -47,6 +71,36 @@ function Users() {
         <div className="card">
           <div className="card-body">
             <h5 className="card-title">List User</h5>
+            <div className="datatable-top">
+              {/* Seach box */}
+              <div className="datatable-search">
+                <input
+                  className="datatable-input"
+                  placeholder="Search..."
+                  type="search"
+                  title="Search within table"
+                  value={search}
+                  onChange={(ev) => {
+                    handleSearch(ev.target.value);
+                  }}
+                />
+              </div>
+
+              {/* Pagination  */}
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "right",
+                  alignItems: "center",
+                }}
+              >
+                <PaginationComponent
+                  pageCount={pageCount}
+                  currentPage={currentPage - 1}
+                  handlePageChange={handlePageChange}
+                />
+              </div>
+            </div>
             <table className="table table-striped">
               <thead>
                 <tr>
@@ -71,32 +125,34 @@ function Users() {
               )}
 
               {!loading && (
-                <tbody>
-                  {Array.isArray(users) &&
-                    users.map((user) => (
-                      <tr key={user.id}>
-                        <th scope="row">{user.id}</th>
-                        <td>{user.name}</td>
-                        <td>{user.email}</td>
-                        <td>{user.created_at}</td>
-                        <td>
-                          <Link
-                            to={`/admin/users/${user.id}`}
-                            className="btn btn-outline-secondary"
-                          >
-                            Edit
-                          </Link>
-                          &nbsp;
-                          <button
-                            onClick={(ev) => onDeleteUser(user.id)}
-                            className="btn btn-outline-danger"
-                          >
-                            Delete
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
+                <>
+                  <tbody>
+                    {Array.isArray(users) &&
+                      users.map((user) => (
+                        <tr key={user.id}>
+                          <th scope="row">{user.id}</th>
+                          <td>{user.name}</td>
+                          <td>{user.email}</td>
+                          <td>{user.created_at}</td>
+                          <td>
+                            <Link
+                              to={`/admin/users/${user.id}`}
+                              className="btn btn-outline-secondary"
+                            >
+                              Edit
+                            </Link>
+                            &nbsp;
+                            <button
+                              onClick={(ev) => onDeleteUser(user.id)}
+                              className="btn btn-outline-danger"
+                            >
+                              Delete
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </>
               )}
             </table>
           </div>
